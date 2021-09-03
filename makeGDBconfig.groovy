2f4e78745da947dedd5f18885ead543c96b4120c
Egon Willighagen <egon.willighagen@gmail.com>
2021-09-03 18:32:33 +0200

    Added a script to generate gdb.config content

---
+++ makeGDBconfig.groovy
@@ -0,0 +1,31 @@
+// Copyright (C) 2021  Egon Willighagen
+// License: MIT
+
+import groovy.json.JsonSlurper
+
+templateFile = "gene_database/template.md"
+
+// create the GDB config file content
+lines = new File(templateFile).readLines()
+lines.each { String line ->
+  if (line.startsWith("<files>")) {
+    def instruction = new XmlSlurper().parseText(line)
+    def input = instruction.text()    
+    def jsonSlurper = new JsonSlurper()
+    fileContents = new File(input).text
+    new File("${input}.config").withWriter('utf-8') { writer ->
+      def data = jsonSlurper.parseText(fileContents)
+      data.mappingFiles.each { mappingFile ->
+        if (data.type == "Species") {
+          if (mappingFile.species == "Human Coronaviruses") { // exception
+            writer.writeLine "*\t${mappingFile.file}"
+          } else {
+            writer.writeLine "${mappingFile.species}\t${mappingFile.file}"
+          }
+        } else {
+          writer.writeLine "*\t${mappingFile.file}"
+        }
+     }
+    }
+  }
+}
